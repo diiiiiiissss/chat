1c3cdcfadff2f2dc069dfbf2984dcd517d5885f0
diiiiiiissss <rozhkodiana31072012@gmail.com>
2026-05-10 15:57:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-alert('Welcome to my chat!')
+
+const socket = io();
+const messages = document.getElementById('messages');
+const form = document.getElementById('f');
+const input = document.getElementById('input');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (input.value) {
+        socket.emit('new_message', input.value);
+        input.value = '';
+    }
+});
+
+socket.on('new_message', function(msg) {
+    let item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+});
+
+function changeNickname() {
+    let nickname = prompt('Choose your nickname:');
+    if (nickname) {
+        socket.emit('change_nickname', nickname);
+    }
+}
+
+changeNickname();
